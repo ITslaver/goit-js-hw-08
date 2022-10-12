0831ee5cdf18c3f0551bc0b4a3fcff7b6a40277a
Viacheslav Polishchuk <mr_v.p@ukr.net>
2022-10-12 13:08:19 +0300

    add 01, 02
    01-gallery and 02-video at work

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,20 @@
 // Add imports above this line
-import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import renderMarkup from './customFunction/renderMarkup';
 // Change code below this line
-// console.log(SimpleLightbox);
 
+console.log(renderMarkup);
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMakrup = renderMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMakrup);
+
+const lightboxGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+console.log(lightboxGallery);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+function onCurrentTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', throttle(onCurrentTime, 1000));
+
+function onPlayTime(data) {
+  const stopedTime = localStorage.getItem('videoplayer-current-time');
+  if (data.seconds !== stopedTime) {
+    player.setCurrentTime(stopedTime);
+  }
+}
+
+player.on('play', onPlayTime);
